29fa23834199c8af9c5e44378e69097b3be4d94a
Slayer <90118444+ThePonySlay@users.noreply.github.com>
2021-12-12 08:53:22 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -1,8 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
+/*Constantes */
+#define TERMINAL "st"
+#define TERMCLASS "ST"
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 20;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;           /* 0: systray in the right corner, >0: systray on left of status text */
@@ -11,8 +15,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=18" };
-static const char dmenufont[]       = "monospace:size=18";
+static const char *fonts[]          = { "monospace:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#ffffff";
@@ -64,20 +68,25 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
 static const char *libre[] = { "librewolf", NULL };
 static const char *flame[] = { "flameshot", NULL };
 static const char *pcman[] = { "pcmanfm", NULL };
-static const char *spyware[] = { "discord", NULL };
+static const char *element[] = { "element-desktop", NULL };
 static const char *xkill[] = { "xkill", NULL };
 static const char *slock[] = { "slock", NULL };
+static const char *freetube[] = { "freetube", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ShiftMask,             XK_u,      spawn,         SHCMD(TERMINAL " htop") },
+	{ MODKEY,                       XK_n,      spawn,         SHCMD(TERMINAL " ranger") },
+	{ MODKEY,                       XK_m,      spawn,         SHCMD(TERMINAL " cmus") },
+	{ MODKEY,                       XK_y,      spawn,          {.v = freetube } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = xkill } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = spyware } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = element } },
 	{ MODKEY,                       XK_e,      spawn,          {.v = pcman } },
 	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = flame } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = libre } },
@@ -87,16 +96,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_6,      view,           {.ui = ~0 } },
